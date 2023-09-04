61fd5f3b12e9e87b1d333ccf571cf85b525b64b0
abhayr20 <abhay.rastogi20@alumni.imperial.ac.uk>
2023-09-04 16:55:12 +0530

    phyloseq.r update

--- scripts/BIC_phyloseq.R
+++ scripts/BIC_phyloseq.R
@@ -1,10 +1,14 @@
-library(BiocManager)
-library(phyloseq)
+library("tiidyverse")
+library("ggplot2")      
+library("readxl")       
+library("dplyr")        
+library("tibble") 
 
 setwd("~/Desktop/AIIMS/BIC_microbiome")
 
 ######################################################
 # Phyloseq installation and documentation
+# library(BiocManager)
 # BiocManager::install("phyloseq")
 # browseVignettes("phyloseq") 
 ######################################################
@@ -23,5 +27,35 @@ OTU = otu_table(otu_mat, taxa_are_rows = TRUE)
 TAX = tax_table(tax_mat)
 samples = sample_data(df_mat)
 
-carbom <- phyloseq(OTU, TAX, samples)
-carbom
+bic <- phyloseq(OTU, TAX, samples)
+
+
+# Normalize number of reads in each sample using median sequencing depth.
+# The number of reads used for normalization is **`r sprintf("%.0f", total)`**. 
+total = median(sample_sums(bic))
+standf = function(x, t=total) round(t * (x / sum(x)))
+bic = transform_sample_counts(bic, standf)
+
+# beta-diversity Clusterinig
+ndms_distance <- ordinate(physeq=bic, method="NMDS", distance = "bray") #nmds
+ordinate.df <- as.data.frame(ndms_distance$points)
+ordinate.df$race <- as.factor(df_mat$race)
+
+ggplot(ordinate.df) +
+  geom_point(aes(x=MDS1, y=MDS2, col=race), size = 2)
+
+pcoa_dist <- ordinate(physeq=bic, method="PCoA", distance = "bray") 
+pcoa.df <- as.data.frame(pcoa_dist$vectors)
+pcoa.df$race <- as.factor(df_mat$race)
+
+ggplot(pcoa.df) +
+  geom_point(aes(x=Axis.1, y=Axis.2, col=race), size = 2)
+
+#Rtsne:
+
+# Alpha-diversity clustering
+alpha_diversity<- estimate_richness(bic, split = TRUE, measures = NULL)
+
+
+  
+  
